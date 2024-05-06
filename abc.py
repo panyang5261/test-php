000334d8212cf24216fba80ce71472afda7a0c01
pan yang <yang.pan@send2china.co.uk>
2024-05-06 16:23:18 +0800

    Revert "add revert 2"
    This reverts commit dedc31eda4fa92fdc49f8459e1e51de692af60bf.

--- abc.py
+++ abc.py
@@ -4,11 +4,3 @@ def test():
 
 def revert1():
     print('it is a good time, rever 1')
-
-
-def revert2():
-    print('it is a good time, rever 2')
-
-
-def revert3():
-    print('it is a good time, rever 3')
